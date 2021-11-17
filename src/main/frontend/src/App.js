d5a747ea77ab047ab255c68de1ee88ebed3fd911
Aydar Fayzullin <faidarik@mail.ru>
2021-11-17 01:49:41 -0500

    Create Login and TopArtists view routes

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Login from './components/Login';
+import TopArtists from "./components/TopArtists";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Login/>
+      <Routes>
+        <Route exact path="/" element={<Login/>} />
+        <Route exact path="/top-artists" element={<TopArtists/>} />
+      </Routes>
     </div>
   );
 }
--- src/main/frontend/src/components/TopArtists.js
+++ src/main/frontend/src/components/TopArtists.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react'
+import { Link } from "react-router-dom"
 
 function TopArtists() {
     const [userTopArtists, setUserTopArtists] = useState();    
 
     useEffect(() => {
-        fetch("http://localhost:8000/api/user-top-artists")
+        fetch("http://localhost:8080/api/user-top-artists")
         .then(res => res.json())
         .then(data => {
             console.log(data);
@@ -22,8 +23,9 @@ function TopArtists() {
             (
                 <h1>LOADING...</h1>
             )}
+            <Link to="/">Home</Link>
         </div>
     )
 }
 
-export default TopArtists
+export default TopArtists;
