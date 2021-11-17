e190aa28d92dcca2d34126360d52c3edb85af033
Aydar Fayzullin <faidarik@mail.ru>
2021-11-16 22:11:32 -0500

    Add user-top-artists and user-top-songs routes

---
+++ src/main/java/com/aydarfz/spotifyclone/controller/SpotifyApiController.java
@@ -0,0 +1,74 @@
+package com.aydarfz.spotifyclone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.michaelthelin.spotify.model_objects.specification.Artist;
+import se.michaelthelin.spotify.model_objects.specification.Paging;
+import se.michaelthelin.spotify.model_objects.specification.Playlist;
+import se.michaelthelin.spotify.model_objects.specification.Track;
+import se.michaelthelin.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
+import se.michaelthelin.spotify.requests.data.personalization.simplified.GetUsersTopTracksRequest;
+import se.michaelthelin.spotify.requests.data.playlists.GetListOfUsersPlaylistsRequest;
+
+import static com.aydarfz.spotifyclone.controller.AuthController.spotifyApi;
+
+@RestController
+@RequestMapping("/api")
+public class SpotifyApiController {
+    @GetMapping(value = "user-top-artists")
+    public Artist[] getUserTopArtists() {
+
+        final GetUsersTopArtistsRequest getUsersTopArtistsRequest = spotifyApi.getUsersTopArtists()
+                .time_range("medium_term")
+                .limit(10)
+                .offset(5)
+                .build();
+
+        try {
+            final Paging<Artist> artistPaging = getUsersTopArtistsRequest.execute();
+
+            // return top artists as JSON
+            return artistPaging.getItems();
+        } catch (Exception e) {
+            System.out.println("Something went wrong!\n" + e.getMessage());
+        }
+        return new Artist[0];
+    }
+
+    @GetMapping(value = "user-top-songs")
+    public Track[] getUserTopTracks() {
+
+        final GetUsersTopTracksRequest getUsersTopTracksRequest = spotifyApi.getUsersTopTracks()
+                .time_range("medium_term")
+                .limit(10)
+                .offset(5)
+                .build();
+
+        try {
+
+            final Paging<Track> trackPaging = getUsersTopTracksRequest.execute();
+
+            // return top Tracks as JSON
+            return trackPaging.getItems();
+        } catch (Exception e) {
+            System.out.println("Something went wrong!\n" + e.getMessage());
+        }
+        return new Track[0];
+    }
+
+//    @GetMapping(value = "user-playlists")
+//    public Playlist[] getUserPlaylists() {
+//
+//        final GetListOfUsersPlaylistsRequest getListOfUsersPlaylistsRequest = spotifyApi.getListOfUsersPlaylists();
+//
+//        try {
+//            final Paging<Playlist> playlistPaging = getListOfUsersPlaylistsRequest.execute();
+//            // return Playlists as JSON
+//            return playlistPaging.getItems();
+//        } catch (Exception e) {
+//            System.out.println("Something went wrong!\n" + e.getMessage());
+//        }
+//        return new Playlist[0];
+//    }
+}
