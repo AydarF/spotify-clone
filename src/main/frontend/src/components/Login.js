7488b534eb627ac380f73c41b892605a6eb3222b
Aydar Fayzullin <faidarik@mail.ru>
2021-11-16 22:36:38 -0500

    Connect Login page with Spotify authorization page

--- src/main/frontend/src/components/Login.js
+++ src/main/frontend/src/components/Login.js
@@ -2,10 +2,18 @@ import React from 'react'
 import "./Login.css"
 
 function Login() {
+    const getSpotifyUserLogin = () => {
+        fetch("http://localhost:8080/api/login")
+        .then((response) => response.text())
+        .then(response => {
+            window.location.replace(response);
+        })
+    }
+    
     return (
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify Logo" />
-            <a href="/#">LOGIN WITH SPOTIFY</a>
+            <button onClick={getSpotifyUserLogin}>LOGIN WITH SPOTIFY</button>
         </div>
     )
 }
