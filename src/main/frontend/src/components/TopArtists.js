ee63f0fe360993368970149323ec08aa6ef4c241
Aydar Fayzullin <faidarik@mail.ru>
2021-11-26 23:54:32 -0500

    Style TopArtists component

--- src/main/frontend/src/components/TopArtists.js
+++ src/main/frontend/src/components/TopArtists.js
@@ -1,5 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from "react-router-dom"
+// import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem'
+import ImageListItemBar from '@mui/material/ImageListItemBar'
+import './TopArtists.css'
+
 
 function TopArtists() {
     const [userTopArtists, setUserTopArtists] = useState();    
@@ -14,16 +19,30 @@ function TopArtists() {
     }, [])
 
     return (
-        <div>
+        <div className="topArtists">
+            <ul>
             { userTopArtists ? (
                 userTopArtists.map((artist) => {
-                    return <h1 key= {artist.name}>{artist.name}</h1>
+                    return (                        
+                        <ImageListItem key={artist.name}>
+                            <img  
+                                src={artist.images[0].url} 
+                                srcSet={artist.images[0].url}
+                                alt={artist.name} loading="lazy" />                                                
+                            <ImageListItemBar
+                                sx={{textAlign: "center", marginTop: "-6px", backgroundColor: "white", color: "black", borderBottomRightRadius: "0.25rem", borderBottomLeftRadius: "0.25rem"}}
+                                title={artist.name}            
+                                position="below"
+                            />
+                        </ImageListItem> 
+                    )
                 })
             ):
             (
                 <h1>LOADING...</h1>
             )}
             <Link to="/">Home</Link>
+            </ul>
         </div>
     )
 }
