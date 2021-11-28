ef133461b689e9c9e5485647b5cbb1cdc31a9db8
Aydar Fayzullin <faidarik@mail.ru>
2021-11-27 22:07:29 -0500

    Create TopSongs.css file and style TopSongs component

--- src/main/frontend/src/components/TopSongs.js
+++ src/main/frontend/src/components/TopSongs.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import ImageListItem from '@mui/material/ImageListItem'
+import ImageListItemBar from '@mui/material/ImageListItemBar'
+import './TopArtists.css'
+import './TopSongs.css'
+
 
 function TopSongs() {
     const [ userTopSongs, setTopSongs ] = useState();
@@ -14,15 +19,35 @@ function TopSongs() {
     }, [])
 
     return (
-        <div>
-            {userTopSongs ? (
-                userTopSongs.map(song => {
-                    return <p key={song.name}>{song.name}</p>
-                })
-            ) : (
-                <h1>Loading...</h1>
+        <div className="main">
+            <ul>
+                {userTopSongs ? (
+                    userTopSongs.map((song) => {
+                        return (                        
+                            <ImageListItem 
+                                key={song.name}
+                                className="imageListItem">                            
+                                <img  
+                                    src={song.album.images[0].url} 
+                                    srcSet={song.album.images[0].url}
+                                    alt={song.name} loading="lazy" />                                                
+                                <ImageListItemBar
+                                    className="imageListItemBar"
+                                    title={song.name}
+                                    subtitle={
+                                        <>
+                                            <h3 className="subtitle">{song.album.artists[0].name}</h3>
+                                        </>}                                                          
+                                    position="below"
+                                />
+                            </ImageListItem> 
+                        )
+                    })
+                ) : (
+                    <h1>Loading...</h1>
                 )}
-                <Link to="/">Home</Link>
+                    <Link to="/">Home</Link>
+                </ul>
         </div>
     )
 }
