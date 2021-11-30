9d9f8ed37c4c45d952774ddc2d484a97f393fc4a
Aydar Fayzullin <faidarik@mail.ru>
2021-11-29 23:56:48 -0500

    Make header sticky

--- src/main/frontend/src/components/Header.js
+++ src/main/frontend/src/components/Header.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { Link } from "react-router-dom";
+import './Header.css';
 
 function Header({componentPath, componentName}) {
+
+        /* Sticky Menu Area */
+        useEffect(() => {
+            window.addEventListener('scroll', isSticky);
+            return () => {
+                window.removeEventListener('scroll', isSticky);
+            };
+        });
+    
+               
+        /* Method that will fix header after a specific scrollable */
+        const isSticky = (e) => {
+            const header = document.querySelector('.header-section');
+            const scrollTop = window.scrollY;
+            scrollTop >= 1 ? header.classList.add('is-sticky') : header.classList.remove('is-sticky');
+        };
+
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-around', gap: '50px', padding: '50px 0 20px', bgcolor: 'background.paper', }}>
-            <Box sx={{display: 'flex', justifyContent: 'space-around', gap: '50px'}}>      
+        <Box className="header-section">
+            <Box className="header-section-left">      
                 <Link to="/" >                                   
-                    <Grid container sx={{ color: 'text.primary', gap: '10px' }}>                                                   
+                    <Grid container>                                                   
                         <Typography>Home</Typography>                        
                     </Grid>
                 </Link>
                 <Link to={componentPath} style={{display: "block"}}>
-                    <Grid container sx={{ color: 'text.primary', gap: '10px' }}>                                                         
+                    <Grid container>                                                         
                         <Typography>{componentName}</Typography>                                
                     </Grid>                
                 </Link>
             </Box>
             <Link to="/" >                
-                <Grid container sx={{ color: 'text.primary', gap: '10px' }}>                        
+                <Grid container>                        
                     <Typography>Logout</Typography>
                 </Grid>
             </Link>                
