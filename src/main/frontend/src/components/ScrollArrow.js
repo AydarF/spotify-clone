71d78cbc6fb8e68c3094500757316db11fb8c56e
Aydar Fayzullin <faidarik@mail.ru>
2021-12-08 19:56:29 -0500

    Refactor ScrollArrow to use useEffect hook in checkScrollTop function for better performance

--- src/main/frontend/src/components/ScrollArrow.js
+++ src/main/frontend/src/components/ScrollArrow.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 import './ScrollArrow.css';
 
@@ -7,19 +7,26 @@ const ScrollArrow = () =>{
 
   const [showScroll, setShowScroll] = useState(false)
 
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 50){
-      setShowScroll(true)
-    } else if (showScroll && window.pageYOffset <= 50){
-      setShowScroll(false)
-    }
-  };
-
   const scrollTop = () =>{
     window.scrollTo({top: 0, behavior: 'smooth'});
   };
 
-  window.addEventListener('scroll', checkScrollTop)
+  useEffect(() => {
+    const checkScrollTop = () => {
+      if (!showScroll && window.pageYOffset > 50){
+        setShowScroll(true)
+      } else if (showScroll && window.pageYOffset <= 50){
+        setShowScroll(false)
+      }
+    };
+
+    window.addEventListener('scroll', checkScrollTop);
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop);
+    };
+  }, [showScroll]);
+
+ 
 
   return (
         <ArrowCircleUpIcon style={{display: showScroll ? 'flex' : 'none'}} sx={{fontSize: '2.5rem'}} className="scrollTop" onClick={scrollTop} />
