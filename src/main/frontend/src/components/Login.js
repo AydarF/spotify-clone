6a3f3864a674e93bbe31a8c45221ae71c8264038
Aydar Fayzullin <faidarik@mail.ru>
2021-12-08 19:47:18 -0500

    Update spotify logo source

--- src/main/frontend/src/components/Login.js
+++ src/main/frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Login.css"
+import spotifyImg from '../resources/images/spotifyImg.png'
 
 function Login() {
     const getSpotifyUserLogin = () => {
@@ -12,7 +13,7 @@ function Login() {
     
     return (
         <div className="login">
-            <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify Logo" />
+            <img src={spotifyImg} alt="Spotify Logo" />
             <button onClick={getSpotifyUserLogin}>LOGIN WITH SPOTIFY</button>
         </div>
     )
